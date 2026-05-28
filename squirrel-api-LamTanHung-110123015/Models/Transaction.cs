42d5301781dcf8eba09c977e9feef704bdaf37e5
lamhung2k5 <147480226+lamhung2k5@users.noreply.github.com>
2026-05-28 06:46:20 +0000

    Hoan thanh cau 2

---
+++ squirrel-api-LamTanHung-110123015/Models/Transaction.cs
@@ -0,0 +1,10 @@
+namespace squirrel_api_LamTanHung_110123015.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string SoTaiKhoan { get; set; }
+        public double SoTien { get; set; }
+        public string LoaiGiaoDich { get; set; }
+    }
+}
